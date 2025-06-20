2ea6b22f379c53da4cb25e0ff0261d3552eb3e5b
erick <erickcovisfero@gmail.com>
2025-06-20 09:46:39 -0500

    5 modulos completados

--- taller/apy/models.py
+++ taller/apy/models.py
@@ -1,89 +1,47 @@
 from django.db import models
 
-
+#------ ENTIDAD de TIPO mantenmimiento ---------
 class TipoMantenimiento(models.Model):
     id = models.CharField(max_length=50, unique=True)
     nombre = models.CharField(max_length=50, unique=True)
-    descripcion = models.TextField(blank=True)
 
     def __str__(self):
         return self.nombre
         
-#------ ENTIDAD PRODUCTO ---------
-
+#------ ENTIDAD PRODUCTO --------
 class Mantenimiento(models.Model):
-    equipo = models.ForeignKey( on_delete=models.CASCADE)
-    tipo = models.ForeignKey(TipoMantenimiento, on_delete=models.SET_NULL, null=True)
-    fecha = models.DateField()
-    descripcion = models.TextField()
-    realizado_por = models.CharField(max_length=100)
-    observaciones = models.TextField(blank=True, null=True)
+    fallas = models.TextField()
+    procesos = models.CharField(max_length=50)
+    id_vehiculo = models.ForeignKey(vehiculo, on_delete=models.CASCADE)
+    id_empleado = models.ForeignKey(empleado, on_delete=models.CASCADE)
+    id_tipo_mantenimiento = models.ForeignKey(TipoMantenimiento, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.equipo.nombre} - {self.fecha}"
+        return f"{self.fallas} - {self.id_tipo_mantenimiento}"
     
 class Repuesto(models.Model):
-    id = models.CharField(max_length=50, unique=True)
-    nombre = models.CharField(max_length=100)
-    descripcion = models.TextField(blank=True)
+    id_marca = models.ForeignKey(marca, on_delete=models.CASCADE)
+    categoria = models.CharField(max_length=100)
+    fabricante = models.CharField(max_length=100)
     stock = models.IntegerField(default=0)
-
+    ubicacion = models.CharField(max_length=100)
+    precio = models.IntegerField(default=0)
+    
     def __str__(self):
-        return f"{self.nombre} ({self.codigo})"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        return f"{self.fabricante} ({self.categoria})"
 
 class herramienta(models.Model):
-    id = models.CharField(max_length=50, unique=True)
-    marca = models.CharField(max_length=50)
     nombre = models.CharField(max_length=100)
-    descripcion = models.TextField(blank=True)
+    color = models.CharField(max_length=100)
+    tipo = models.CharField(max_length=100)
+    material = models.CharField(max_length=100)
+    id_marca = models.ForeignKey(marca, on_delete=models.CASCADE)
     stock = models.IntegerField(default=0)
 
     def __str__(self):
-        return f"{self.nombre} ({self.id})"
+        return f"{self.nombre} ({self.tipo})"
 
 class insumos(models.Model):
-    id = models.IntegerField()
     id_marca = models.CharField(max_length=50)
     nombre = models.CharField(max_length=100)
     costo = models.IntegerField()
@@ -91,4 +49,4 @@ class insumos(models.Model):
     stock = models.IntegerField(default=0)
 
     def __str__(self):
-        return f"{self.nombre} ({self.id})"
+        return f"{self.nombre} ({self.stock})"
