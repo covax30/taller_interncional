24f75480ff1e697891440a8633e56e502510079a
Steven <floreschaparrojaviersteven@gmail.com>
2025-06-20 09:56:36 -0500

    Modulos

--- taller/apy/models.py
+++ taller/apy/models.py
@@ -1,125 +1,60 @@
 from django.db import models
 
+# MODULOS STEVEN
 
-class TipoMantenimiento(models.Model):
-    id = models.CharField(max_length=50, unique=True)
-    nombre = models.CharField(max_length=50, unique=True)
-    descripcion = models.TextField(blank=True)
-
-    def __str__(self):
-        return self.nombre
-        
-#------ ENTIDAD PRODUCTO ---------
-
-class Mantenimiento(models.Model):
-    equipo = models.ForeignKey( on_delete=models.CASCADE)
-    tipo = models.ForeignKey(TipoMantenimiento, on_delete=models.SET_NULL, null=True)
-    fecha = models.DateField()
-    descripcion = models.TextField()
-    realizado_por = models.CharField(max_length=100)
-    observaciones = models.TextField(blank=True, null=True)
-
-    def __str__(self):
-        return f"{self.equipo.nombre} - {self.fecha}"
-    
-class Repuesto(models.Model):
-    id = models.CharField(max_length=50, unique=True)
+class Cliente(models.Model):
+    id_cliente = models.IntegerField(unique=True)  
+    id_operacion = models.IntegerField(default=0)
     nombre = models.CharField(max_length=100)
-    descripcion = models.TextField(blank=True)
-    stock = models.IntegerField(default=0)
+    documento = models.BigIntegerField(default=0)
+    telefono = models.BigIntegerField(default=0)
+    correo = models.EmailField(max_length=100)
+    fecha_operacion = models.DateField()
+    monto = models.IntegerField(default=0)
 
     def __str__(self):
-        return f"{self.nombre} ({self.codigo})"  
-    
-#-------------Modulo Administrador-----------
-class Administrador:
-    id_admin = models.AutoField(primary_key=True)
-    nombre = models.CharField(max_length=100)
-    apellidos = models.CharField(max_length=100)
-    identificacion = models.CharField(max_length=20, unique=True)
-    edad = models.IntegerField()
-    correo = models.EmailField(unique=True)
-    telefono = models.CharField(max_length=20)
+        return f"{self.id_cliente} - {self.nombre}"
+
+
+class Vehiculo(models.Model):
+    id_vehiculo = models.IntegerField(unique=True) 
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)  # LLAVE
+    placa = models.CharField(max_length=10, unique=True)
+    modelo_vehiculo = models.CharField(max_length=100)
+    marca_vehiculo = models.CharField(max_length=100)
+    color = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.placa} - {self.marca_vehiculo} - {self.modelo_vehiculo}"
+
+
+class EntradaVehiculo(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)  # LLAVE
+    vehiculo = models.ForeignKey(Vehiculo, on_delete=models.CASCADE)  # LLAVE
     fecha_ingreso = models.DateField()
-    
-    def __str__(self):
-        return f"{self.nombre} {self.apellidos}"
-    
-#------- Gastos--------
-class Gastos(models.Model):
-    id_gasto =  models.AutoField(primary_key = True)  #ID automatico
-    monto= models,models.DecimalField(max_digits=10, decimal_places=2)
-    descripcion = models.TextField() 
-    tipo_gastos=models.CharField(max_length=100)
-             
-#------------Modulo Informes-----------
-class Informes:
-    id_informe = models.AutoField(primary_key=True)
-    repuestos_usados = models.TextField()
-    costo_mano_obra = models.DecimalField(max_digits=10, decimal_places=2)
-    fecha = models.DateField()
-    hora = models.TimeField()
-    id_repuesto = models.ForeignKey(Repuesto, on_delete=models.CASCADE)
-    id_empleado = models.ForeignKey(Empleado, on_delete=models.CASCADE)
-    tipo_informe = models.CharField(max_length=100)
-    id_mantenimiento = models.ForeignKey(Mantenimiento, on_delete=models.CASCADE)
-    
-    def __str__(self):
-        return f"{self.id_informe} {self.tipo_informe}"
-    
-class Herramienta(models.Model):
-    id = models.CharField(max_length=50, unique=True)
-    marca = models.CharField(max_length=50)
-    nombre = models.CharField(max_length=100)
-    descripcion = models.TextField(blank=True)
-    stock = models.IntegerField(default=0)
+    hora_ingreso = models.TimeField()
 
     def __str__(self):
-        return f"{self.nombre} ({self.id})"
-
-class Insumos(models.Model):
-    id = models.IntegerField()
-    id_marca = models.CharField(max_length=50)
-    nombre = models.CharField(max_length=100)
-    costo = models.IntegerField()
-    tipo = models.CharField(max_length=100)
-    stock = models.IntegerField(default=0)
-
-    def __str__(self):
-        return f"{self.nombre} ({self.id})"
-    
-#--------------Modulo Pago Servicio Publicos-----------
-class PagoServiciosPublicos:
-    id_servicio = models.AutoField(primary_key=True)
-    monto = models,models.DecimalField(max_digits=10, decimal_places=2)
-    
-    def __str__(self):
-        return f"{self.id_servicio} {self.monto}"
-    
-#--------------Modulo Proveedores--------------
-class Proveedores:
-    id_proveedor = models.AutoField(primary_key=True)
-    nombre = models.CharField(max_length=100)
-    telefono = models.CharField(max_length=20)
-    correo = models.EmailField(unique=True)
-    
-    def __str__(self):
-        return f"{self.id_proveedor} {self.correo}"
-    
-#--------------Modulo Pagos-----------------
-class Pagos:
-    id_pago = models.AutoField(primary_key=True)
-    tipo_pago = models.CharField(max_length=100)
-    fecha = models.DateField()
-    hora = models.TimeField()
-    monto = models,models.DecimalField(max_digits=10, decimal_places=2)
-    id_proveedor = models.ForeignKey(Proveedores, on_delete=models.CASCADE)
-    id_admin = models.ForeignKey(Administrador, on_delete=models.CASCADE)
-    id_herramienta = models.ForeignKey(Herramienta, on_delete=models.CASCADE)
-    id_insumos = models.ForeignKey(Insumos, on_delete=models.CASCADE)
-    id_repuestos = models.ForeignKey(Repuesto, on_delete=models.CASCADE)
-    
-    def __str__(self):
-        return f"{self.id_pago} {self.monto}"
+        return f"{self.cliente.id_cliente} - {self.vehiculo.placa} - {self.fecha_ingreso} {self.hora_ingreso}"
 
 
+class SalidaVehiculo(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)  # LLAVE
+    vehiculo = models.ForeignKey(Vehiculo, on_delete=models.CASCADE)  # LLAVE
+    diagnostico = models.CharField(max_length=100)
+    fecha_ingreso = models.DateField()
+    hora_ingreso = models.TimeField()
+
+    def __str__(self):
+        return f"{self.cliente.id_cliente} - {self.vehiculo.placa} - {self.diagnostico}"
+
+
+class Compra(models.Model):
+    id_factura_compra = models.IntegerField(unique=True)
+    id_compra = models.IntegerField(default=0)
+    proveedor = models.CharField(max_length=100)
+    fecha_compra = models.DateField()
+    hora_compra = models.TimeField()
+
+    def __str__(self):
+        return f"{self.id_factura_compra} - {self.proveedor} - {self.fecha_compra} {self.hora_compra}"
