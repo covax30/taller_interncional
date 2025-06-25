b43e509dae345488c90ae5bbf1fff672e444bcb6
erick <erickcovisfero@gmail.com>
2025-06-25 06:46:10 -0500

    casi

--- taller/apy/views.py
+++ taller/apy/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django .http import HttpResponse
 # Create your views here.
+def vista1(request):
+    return HttpResponse('Hola esta es mi primera vista')
+
+def vista2(request):
+    persona = {
+        'Nombre': 'Karol',
+        'Apellido': 'Talero'
+    }
+    return JsonResponse(persona)
+def vista3(request):
+    return render(request, 'index.html')
