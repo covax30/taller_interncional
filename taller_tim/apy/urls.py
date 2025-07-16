b991cf6bb7f564d6471ae6cc36b823b8162ecc19
Karol Talero <karoltalerolopez@gmail.com>
2025-07-16 15:37:48 -0500

    Agregar vistas de factura y proveedor, nuevos templates y librerías estáticas

---
+++ taller_tim/apy/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from apy.views import *
+from apy.view.Contenidos.views import *
+from apy.view.proveedor.view import *
+
+app_name = 'apy'
+
+urlpatterns = [
+    # --------------urls Karol---------------
+    #path('Modulo_Factura/',  factura, name = 'contenido.factura'),
+    #path('Contactanos/',  contacto, name = 'contenido.contacto'),
+    path('factura/listar/', FacturaListView.as_view() , name='factura_lista'),
+    path('factura/agregar/', FacturaCreateView.as_view(), name='factura_crear'),
+    path('factura/editar/<int:pk>/', FacturaUpdateView.as_view(), name='factura_editar'),
+    path('factura/eliminar/<int:pk>/', FacturaDeleteView.as_view(), name='factura_eliminar'),
+    path('Proveedor/listar/', ProveedorListView.as_view() , name='proveedor_lista'),
+    path('Proveedor/agregar/', ProveedorCreateView.as_view() , name='proveedor_crear'),
+    path('Proveedor/editar/<int:pk>/', ProveedorUpdateView.as_view() , name='proveedor_editar'),
+    path('Proveedor/eliminar/<int:pk>/', ProveedorDeleteView.as_view() , name='proveedor_eliminar')
+] 
