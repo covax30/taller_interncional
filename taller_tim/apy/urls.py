536a1c8d9bcd457070e60ca36c32b7661325aa1f
erick <erickcovisfero@gmail.com>
2025-07-17 06:44:49 -0500

    creacion estructura para implementar bootstrap

--- taller_tim/apy/urls.py
+++ taller_tim/apy/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from apy.views import *
+from .Views import *
 from apy.Views.contenido.views import *
+from apy.Views.fixed_sidebar import *
 
 app_name = 'apy'
 
 urlpatterns = [
     # --------------urls Karol---------------
     path('Modulo_Factura/',  factura, name = 'contenido.factura'),
-    path('Contactanos/',  contacto, name = 'contenido.contacto')
+    path('Contactanos/',  contacto, name = 'contenido.contacto'),
+    path('index/', index.as_view(), name = 'contenido.index'),
 ] 
