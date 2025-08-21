1dd247f07cfc919470dd837012a672e28b9b7129
Steven <floreschaparrojaviersteven@gmail.com>
2025-08-21 09:59:04 -0500

    Fuentes SANS AdminLTE 4.0.0.rc5

--- taller_tim/taller/settings.py
+++ taller_tim/taller/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'apy' / 'static',
+    BASE_DIR / 'apy' / 'static' / 'AdminLTE' / 'dist',
 ]
 
 # Default primary key field type
