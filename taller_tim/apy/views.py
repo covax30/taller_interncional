6ccd02b734d26bf1e54894c85e8ee62079074b12
erick <tucorreo@gmail.com>
2025-10-03 08:18:05 -0500

    eliminar carpeta

--- taller_tim/apy/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse,JsonResponse
-from apy.models import *
-
-# Create your views here.
-
--- taller_tim/login/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from .views import Login_view, logout_redirect, forgot_view
-
-app_name = 'login'
-urlpatterns = [
-
-    path('', Login_view.as_view(), name="login"),
-    path('logout/', logout_redirect.as_view(), name="logout")
-]
