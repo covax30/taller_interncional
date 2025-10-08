7616eaf8a1d8142c595393f96541c6e8a68ac1f8
erick <tucorreo@gmail.com>
2025-10-08 08:19:05 -0500

    guardado avancez accesecibilidad

--- apy/view/insumos/views.py
+++ apy/view/insumos/views.py
@@ -5,6 +5,7 @@ from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_protect, csrf_exempt
 from django.urls import reverse_lazy
 from apy.forms import *
+from django.contrib import messages
 
 # Create your views here.
 # --------------Vistas erick---------------
@@ -29,6 +30,10 @@ class InsumoCreateView(CreateView):
     form_class = InsumoForm
     template_name = 'insumos/crear.html'
     success_url = reverse_lazy('apy:insumo_lista')
+    
+    def form_valid(self, form):
+        messages.success(self.request, "insumo creado correctamente")
+        return super().form_valid(form)  
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
