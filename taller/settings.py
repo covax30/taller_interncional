ff04875dec87387752e9fd3810827d65f0f80ae2
covax30 <erickcovisfero@gmail.com>
2026-03-02 07:15:20 -0500

    arreglo emergecia settings

--- taller/settings.py
+++ taller/settings.py
@@ -223,14 +223,13 @@ LOGOUT_REDIRECT_URL = ''
 # -----------------------------------------------------
 # ✅ CONFIGURACIÓN DE CORREO
 # -----------------------------------------------------
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-
+EMAIL_HOST_USER = 'soportecnico.t.i.m@gmail.com'
+EMAIL_HOST_PASSWORD = 'aocumubtnqxvccbb'
 
 # -----------------------------------------------------
 # 📦 WhiteNoise y Almacenamiento
